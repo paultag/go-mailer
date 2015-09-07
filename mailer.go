4e32c03255675532bc0debf65998257a221a9c18
Paul Tagliamonte <paultag@gmail.com>
2015-09-07 14:33:14 -0400

    Add a sanity wrapper.

---
+++ mailer.go
@@ -0,0 +1,63 @@
+package mailer
+
+import (
+	"bytes"
+	"fmt"
+	"path"
+
+	"net/smtp"
+	"pault.ag/go/config"
+	"text/template"
+)
+
+type MailerRC struct {
+	Sender   string `flag:"mailer-sender"    description:"SMTP Sender"`
+	Password string `flag:"mailer-password"  description:"SMTP Password"`
+	Host     string `flag:"mailer-server"    description:"SMTP Server"`
+	Port     int    `flag:"mailer-port"      description:"SMTP Port"`
+}
+
+type Mailer struct {
+	Config MailerRC
+	Root   string
+}
+
+func (m *Mailer) Mail(to []string, mailTemplate string, data interface{}) error {
+	auth := smtp.PlainAuth(
+		"",
+		m.Config.Sender,
+		m.Config.Password,
+		m.Config.Host,
+	)
+
+	byteBuffer := bytes.Buffer{}
+
+	t, err := template.ParseFiles(path.Join(m.Root, mailTemplate))
+	if err != nil {
+		return err
+	}
+
+	if err := t.Execute(&byteBuffer, data); err != nil {
+		return err
+	}
+
+	err = smtp.SendMail(
+		fmt.Sprintf("%s:%d", m.Config.Host, m.Config.Port),
+		auth,
+		m.Config.Sender,
+		to,
+		byteBuffer.Bytes(),
+	)
+	return err
+}
+
+func NewMailer(root string) (*Mailer, error) {
+	mailerRC := MailerRC{}
+	if err := config.Load("mailer", &mailerRC); err != nil {
+		return nil, err
+	}
+	return &Mailer{
+		Config: mailerRC,
+		Root:   root,
+	}, nil
+}
